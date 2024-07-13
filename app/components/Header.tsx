621a05aeaef4c3c3d7890c54234b8222a500e598
anujbhatt-dev <anujbhatt023@gmail.com>
2024-07-14 01:30:26 +0530

    fix(build): html entity error

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 export default function Header() {
   return (
-    <h1 className='text-white text-4xl font-bold text-center pt-5'>Cowan's Game of Life</h1>
+    <h1 className= "text-white text-4xl font-bold text-center pt-5">Cowan&apos;s Game of Life</h1>
   )
 }
